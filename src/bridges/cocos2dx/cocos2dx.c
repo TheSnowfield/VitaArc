25aa65bfc4f77fe05c17a028e5dc3c8ea4a82edc
TheSnowfield <thesnowfield@moezsoft.com>
2021-05-08 03:38:54 +0800

    cocox2dx bridge

---
+++ src/bridges/cocos2dx/cocos2dx.c
@@ -0,0 +1,6 @@
+#include "cocos2dx.h"
+
+void bridgePatchCocos2DX(HSOLIB hSoLibrary)
+{
+  
+}
---
+++ src/bridges/cocos2dx/cocos2dx.h
@@ -0,0 +1,8 @@
+#ifndef _BRIDGE_COCOS2DX_H_
+#define _BRIDGE_COCOS2DX_H_
+
+#include <solibrary/solib.h>
+
+void bridgePatchCocos2DX(HSOLIB hSoLibrary);
+
+#endif /* _BRIDGE_COCOS2DX_H_ */

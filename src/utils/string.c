69fd980b76ea5c1a3de9d3c8bd5a5e40e64ed22a
TheSnowfield <thesnowfield@moezsoft.com>
2021-05-07 09:07:20 +0800

    get file name from path

--- src/utils/string.c
+++ src/utils/string.c
@@ -1,6 +1,19 @@
+#include <string.h>
+#include <psp2/types.h>
+
 #include "string.h"
 
-char *utilGetFileName(const char *szFilePath)
+const char *utilGetFileName(const char *szFilePath)
 {
-  
+  uint32_t strLength = strlen(szFilePath);
+
+  for (int i = strLength; i > 0; --i)
+  {
+    if (szFilePath[i] == '/' && i < strLength)
+    {
+      return &szFilePath[i + 1];
+    }
+  }
+
+  return NULL;
 }
--- src/utils/string.h
+++ src/utils/string.h
@@ -1,6 +1,6 @@
 #ifndef _UTILS_STRING_H_
 #define _UTILS_STRING_H_
 
-char *utilGetFileName(const char *szFilePath);
+const char *utilGetFileName(const char *szFilePath);
 
 #endif /* _UTILS_STRING_H_ */

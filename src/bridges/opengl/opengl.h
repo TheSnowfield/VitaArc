df936f8c8ea04f510bbe146ffeeab4c4d2e9f4bd
TheSnowfield <thesnowfield@moezsoft.com>
2021-05-08 03:39:06 +0800

    opengl bridge

---
+++ src/bridges/opengl/opengl.h
@@ -0,0 +1,8 @@
+#ifndef _BRIDGE_OPENGL_H_
+#define _BRIDGE_OPENGL_H_
+
+#include <solibrary/solib.h>
+
+void bridgePatchGL(HSOLIB lpSoLibrary);
+
+#endif /* _BRIDGE_OPENGL_H_ */
---
+++ src/bridges/opengl/openglimpl.h
@@ -0,0 +1,28 @@
+#ifndef _BRIDGE_OPENGL_IMPL_H_
+#define _BRIDGE_OPENGL_IMPL_H_
+
+#include <vitaGL.h>
+
+void glBindRenderbuffer(GLenum target,
+                        GLuint renderbuffer){};
+
+void glDeleteRenderbuffers(GLsizei n,
+                           GLuint *renderbuffers){};
+
+void glGenRenderbuffers(GLsizei n,
+                        GLuint *renderbuffers) {}
+
+void glFramebufferRenderbuffer(GLenum target,
+                               GLenum attachment,
+                               GLenum renderbuffertarget,
+                               GLuint renderbuffer) {}
+
+void glRenderbufferStorage(GLenum target,
+                           GLenum internalformat,
+                           GLsizei width,
+                           GLsizei height) {}
+
+void glPixelStorei(GLenum pname,
+                   GLint param) {}
+
+#endif _BRIDGE_OPENGL_IMPL_H_

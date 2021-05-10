027a5e1ba032a0c1948a5de9cc998095590c3a51
TheSnowfield <thesnowfield@moezsoft.com>
2021-05-10 14:47:43 +0800

    add log over network

--- src/logcat/logcat.c
+++ src/logcat/logcat.c
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <psp2/io/fcntl.h>
+#include <psp2/net/net.h>
+#include <psp2/net/netctl.h>
+#include <psp2/sysmodule.h>
 
 #include <common/types.h>
 #include <common/define.h>
 #include "logcat.h"
 
-static SceUID logFile;
+#define LOG_OVERUDP
+#define UDP_PORT 23333
+#define UDP_HOST "192.168.1.2"
+
+static SceUID logStream;
 static bool logStarted = false;
 static const char logLvString[] = {'V', 'I', 'W', 'E', 'F'};
+static void *logUdpBuffer = NULL;
 
 void logBegin(const char *logFilePath)
 {
@@ -18,9 +26,44 @@ void logBegin(const char *logFilePath)
     return;
   }
 
+#ifdef LOG_OVERUDP
+
+  if (sceSysmoduleIsLoaded(SCE_SYSMODULE_NET) != SCE_SYSMODULE_LOADED)
+    sceSysmoduleLoadModule(SCE_SYSMODULE_NET);
+
+  if (sceSysmoduleIsLoaded(SCE_SYSMODULE_NET) == SCE_SYSMODULE_LOADED)
+  {
+    SceNetInAddr sNetInAddr = {0};
+    SceNetCtlInfo sNetCtlInfo = {0};
+    SceNetSockaddrIn sNetSockAddr = {0};
+    SceNetInitParam sNetInitParam = {0};
+
+    sNetInitParam.memory = malloc(65535);
+    sNetInitParam.size = 65535;
+    sNetInitParam.flags = 0;
+    sNetSockAddr.sin_family = SCE_NET_AF_INET;
+    sNetSockAddr.sin_port = sceNetHtons(UDP_PORT);
+
+    sceNetInit(&sNetInitParam);
+    sceNetCtlInit();
+    sceNetCtlInetGetInfo(SCE_NETCTL_INFO_GET_IP_ADDRESS, &sNetCtlInfo);
+    sceNetInetPton(SCE_NET_AF_INET, sNetCtlInfo.ip_address, &sNetInAddr);
+    sceNetInetPton(SCE_NET_AF_INET, UDP_HOST, &sNetSockAddr.sin_addr);
+
+    logStream = sceNetSocket("logcat", SCE_NET_AF_INET,
+                             SCE_NET_SOCK_DGRAM, SCE_NET_IPPROTO_UDP);
+
+    sceNetConnect(logStream, &sNetSockAddr, sizeof(sNetSockAddr));
+  }
+
+#else
+
   // Open log file
-  logFile = sceIoOpen(logFilePath, SCE_O_CREAT | SCE_O_WRONLY, 0777);
-  if (logFile <= 0)
+  logStream = sceIoOpen(logFilePath, SCE_O_CREAT | SCE_O_WRONLY, 0777);
+
+#endif
+
+  if (logStream <= 0)
     return;
 
   logStarted = true;
@@ -36,12 +79,19 @@ void logEnd()
   logStarted = false;
   logI(TAG, "Log stop.");
 
-  // Close file
-  sceIoClose(logFile);
+  // Close log
+#ifdef LOG_OVERUDP
+  sceNetSocketClose(logStream);
+#else
+  sceIoClose(logStream);
+#endif
 }
 
 void logBase(LOGLEVEL level, const char *tag, const char *format, va_list args)
 {
+  if (!logStarted)
+    return;
+
   char logBuffer[1024] = {0x00};
   char *logPosition = logBuffer;
 
@@ -49,11 +99,18 @@ void logBase(LOGLEVEL level, const char *tag, const char *format, va_list args)
   logPosition += vsnprintf(logPosition, sizeof(logBuffer), format, args);
   logPosition += snprintf(logPosition, sizeof(logBuffer), "\n");
 
-  sceIoWrite(logFile, logBuffer, logPosition - logBuffer);
+#ifdef LOG_OVERUDP
+  sceNetSend(logStream, logBuffer, logPosition - logBuffer, 0);
+#else
+  sceIoWrite(logStream, logBuffer, logPosition - logBuffer);
+#endif
 }
 
 void inline logPrintf(const char *format, ...)
 {
+  if (!logStarted)
+    return;
+
   char logBuffer[1024] = {0x00};
   char *logPosition = logBuffer;
 
@@ -61,7 +118,11 @@ void inline logPrintf(const char *format, ...)
   va_start(opt, format);
   {
     logPosition += vsnprintf(logPosition, sizeof(logBuffer), format, opt);
-    sceIoWrite(logFile, logBuffer, logPosition - logBuffer);
+#ifdef LOG_OVERUDP
+    sceNetSend(logStream, logBuffer, logPosition - logBuffer, 0);
+#else
+    sceIoWrite(logStream, logBuffer, logPosition - logBuffer);
+#endif
   }
   va_end(opt);
 }

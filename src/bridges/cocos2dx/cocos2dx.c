bd5326fae6e588c7d42255a61a27ab94413d51d4
TheSnowfield <thesnowfield@moezsoft.com>
2021-05-30 02:38:06 +0800

    add patches

--- src/bridges/cocos2dx/cocos2dx.c
+++ src/bridges/cocos2dx/cocos2dx.c
@@ -1,26 +1,31 @@
 #include <utils/patcher.h>
-#include "cocos2dx.h"
+#include <utils/debug.h>
 #include <stdlib.h>
-
-typedef int (*Cocos2dxRendererNativeInit)(uint32_t, uint32_t);
-
-void bridgeCocos2DXRendererNativeInit(HSOLIB hSoLibrary,
-                                      uint32_t nScreenWidth, uint32_t nScreenHeight)
-{
-  Cocos2dxRendererNativeInit lpfnNativeInit =
-      solibGetProcAddress(hSoLibrary, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit");
-
-  lpfnNativeInit(nScreenWidth, nScreenHeight);
-}
+#include "cocos2dx.h"
 
 void bridgePatchCocos2DX(HSOLIB hSoLibrary)
 {
+  void *lpPatchAddress = NULL;
+
   // cocos2d::experimental::FrameBuffer::_frameBuffers
   // read nullptr
-  void *lpPatchAddress = malloc(4);
+  lpPatchAddress = malloc(4);
   patchUint32(hSoLibrary, 0xA5F750, lpPatchAddress);
 
   // cocos2d::JniHelper::deleteLocalRefs [BLX LR]
   // __next_prime underflow exception in unordered_map
   patchARM(hSoLibrary, 0x393B68, 0xE12FFF3E);
+
+  // cocos2d::JniHelper::convert
+  // unordered_map crashed
+  patchThumb(hSoLibrary, 0x5C2848 + 0, 0x00BF);
+  patchThumb(hSoLibrary, 0x5C2848 + 2, 0x00BF);
+  patchThumb(hSoLibrary, 0x5C284C + 0, 0x00BF);
+  patchThumb(hSoLibrary, 0x5C284E + 0, 0x00BF);
+  patchThumb(hSoLibrary, 0x5C2850 + 0, 0x00BF);
+  patchThumb(hSoLibrary, 0x5C2850 + 2, 0x00BF);
+
+  // AudioManager::init
+  // avoid initialize opensles
+  patchThumb(hSoLibrary, 0x50E284, 0x00BF);
 }
